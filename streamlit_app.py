21c81b8807c873bcc45915d2b7831fa9cebf8fe6
mose31 <monamose15@gmail.com>
2024-11-26 20:24:51 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,5 @@ import streamlit as st
 st.title("WTS :blue[WARUNG TEDUH SANTUI] :rice:")
 
 
-import streamlit as st
-st.image("sunrise.jpg", caption="Sunrise by the mountains")
+picture = st.camera_input("Take a picture", disabled=not enable)
 
