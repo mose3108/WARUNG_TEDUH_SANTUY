f336ce64abc10fcf99625ef4882692c70c50f806
mose31 <monamose15@gmail.com>
2024-11-26 20:17:21 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
 import streamlit as st
 
 st.title("WTS :blue[WARUNG TEDUH SANTUI] :rice:")
+
+st.logo(
+    LOGO_URL_LARGE,
+    link="https://streamlit.io/gallery",
+    icon_image=LOGO_URL_SMALL,
+)
