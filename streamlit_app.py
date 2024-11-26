b548ce483797f53472f88716085ee2d582b30941
mose31 <monamose15@gmail.com>
2024-11-26 20:22:45 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit as st
-import streamlit as st
 
 st.title("WTS :blue[WARUNG TEDUH SANTUI] :rice:")
 
+
+import streamlit as st
 st.image("sunrise.jpg", caption="Sunrise by the mountains")
+
