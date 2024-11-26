d589468b72916126aee68c419a3a0c08909b3251
mose31 <monamose15@gmail.com>
2024-11-26 20:34:37 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,16 @@ import streamlit as st
 
 st.title("WTS :blue[WARUNG TEDUH SANTUI] :rice:")
 
-st.logo()
-LOGO_URL_LARGE,
-link="https://streamlit.io/gallery",
-icon_image=LOGO_URL_SMALL,
+import streamlit as st
+
+HORIZONTAL_RED = "images/horizontal_red.png"
+ICON_RED = "images/icon_red.png"
+HORIZONTAL_BLUE = "images/horizontal_blue.png"
+ICON_BLUE = "images/icon_blue.png"
+
+options = [HORIZONTAL_RED, ICON_RED, HORIZONTAL_BLUE, ICON_BLUE]
+sidebar_logo = st.selectbox("Sidebar logo", options, 0)
+main_body_logo = st.selectbox("Main body logo", options, 1)
+
+st.logo(sidebar_logo, icon_image=main_body_logo)
+st.sidebar.markdown("Hi!")
